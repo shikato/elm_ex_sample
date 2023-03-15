c5006d9ce038e017dd242d655f676f85b5e0e8a6
Shinya Kato <Shinya.Kato1@ibm.com>
2023-03-15 15:47:30 +0900

    update comment

--- extensions/js/my_sample2.js
+++ extensions/js/my_sample2.js
@@ -2,18 +2,16 @@ var _selected;
 
 // Artifactが選択されると呼ばれる
 RM.Event.subscribe(RM.Event.ARTIFACT_SELECTED, function(selected) {
-  console.log("selected artifact_");
   console.log("selected", selected);
 
   // 選択したArtifactから指定した属性値の値を読み込む
-	RM.Data.getAttributes(selected[0], 
-		["A", "C", "D",
-		 "B"], function(result) {
-		 	if (result.code === RM.OperationResult.OPERATION_OK) {
-        console.log("result", result);
-				// get the RM.ArtifactAttributes object from the result
-				var attrs = result.data[0];
-				// invoke our processASIL function with that object :
+  RM.Data.getAttributes(selected[0], 
+    ["A", "C", "D", "B"], function(result) {
+      if (result.code === RM.OperationResult.OPERATION_OK) {
+        console.log("getting attr result", result);
+        // get the RM.ArtifactAttributes object from the result
+        var attrs = result.data[0];
+        // invoke our processASIL function with that object :
         console.log("attrs", attrs);
 
         var id = attrs.values["ID"];
@@ -24,19 +22,18 @@ RM.Event.subscribe(RM.Event.ARTIFACT_SELECTED, function(selected) {
         // 変数上で更新した属性値はボタン押下時に使うためグローバル変数に格納
         _selected = attrs;
       }
-	});
+  });
 });
 
 $(function() {
   console.log("set click event");
   // #calcBtnボタンにクリックイベントを定義
   $("#calcBtn").on("click", () => {
-    console.log("on click!");
-    console.log("selected when click", _selected);
+    console.log("on click #calcBtn");
 
     // #calcBtnボタンが押下されたら属性値を更新 
     RM.Data.setAttributes(_selected, function(r) {
-      console.log("save result when click", r);
+      console.log("setting attr result when click", r);
     });
   });
 });
